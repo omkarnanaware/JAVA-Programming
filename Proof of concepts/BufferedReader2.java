3db57bd91bbcc77194d0a817ff7d31a51a60da7c
Omkar Nanaware <omkarnanware1998@gmail.com>
2022-05-26 23:25:18 +0530

    first commit

---
+++ Proof of concepts/BufferedReader2.java
@@ -0,0 +1,33 @@
+import java.lang.*;
+import java.io.*;
+
+
+class BufferedReader2
+{
+    public static void main(String arg[])
+    {
+
+        InputStreamReader iobj = new InputStreamReader(System.in);
+        BufferedReader bobj =  new BufferedReader(iobj);
+    try
+    {
+        System.out.println("Enter your name:");
+        String name =bobj.readLine();
+        System.out.println("Your name is :"+name);
+    }
+    catch(IOException obj)
+    {
+       System.out.println("Exception occurs ");
+    }  
+    
+    finally
+    {
+       iobj = null;
+       bobj = null;
+    }
+        
+        
+
+    }
+
+}
